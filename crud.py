8fb9de1036cf0c66b1a70a3caaa53180e5aa430d
Javad Sadati <118483830+Javad2004@users.noreply.github.com>
2024-06-05 16:39:15 +0330

    Some Minor Modifications

--- crud.py
+++ crud.py
@@ -12,7 +12,7 @@ def get_users(db: Session, skip: int, limit: int = 100):
     return db.query(models.User).order_by(models.User.score.desc()).offset(skip).limit(limit).all()
 
 def get_users_top10(db: Session):
-    return db.query(models.User).order_by(models.User.score).limit(10).all()
+    return db.query(models.User).order_by(models.User.score.desc()).limit(10).all()
 
 def create_user(db: Session, user: schemas.UserCreate):
     db_user = models.User(name=user.name, score=user.score)
